3c2dbd03a1ed079362846f62e9c62f24553d5f72
HuangFuqiang <fuqianghuang01@gmail.com>
2019-04-16 17:13:12 +0800

    [ADD]springboot-redis

---
+++ src/main/java/com/code/springbootredis/dao/repository/UserMapper.java
@@ -0,0 +1,8 @@
+package com.code.springbootredis.dao.repository;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.code.springbootredis.dao.entity.User;
+
+
+public interface UserMapper extends BaseMapper<User> {
+}
